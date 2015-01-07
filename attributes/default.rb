0408b33479c7b4ec08c05528935309f077797f5b
Barry Hoggard <barry@paddle8.com>
2015-01-07 11:44:18 -0500

    update to v0.13

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['remote_syslog2']['install_dir'] = '/usr/local/bin'
-default['remote_syslog2']['version']     = 'v0.10'
+default['remote_syslog2']['version']     = 'v0.13'
 default['remote_syslog2']['filename']    = 'remote_syslog_linux_386.tar.gz'
-default['remote_syslog2']['checksum']    = 'e4fb997a3679832e0130db4faa4050e060e4287e0e73c13da9474108f1ba59c3'
+default['remote_syslog2']['checksum']    = '02ab161eea0cb7a19f7be38b13003bb15ebb72d43f5ca2d6b48b9119d79f5972'
